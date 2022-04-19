844913c4542188877449296cb39dd66a3d22a0ad
unknown <christysimarmata7@gmail.com>
2022-04-19 11:20:48 +0700

    first commit

---
+++ app/src/main/java/com/example/a10120806latihan1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a10120806latihan1;
+
+// Tanggal Pengerjaan : 19 April 2022
+// Nama : Christy Gunawan Simarmata
+// NIM : 10120806
+// Kelas : IF5
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
